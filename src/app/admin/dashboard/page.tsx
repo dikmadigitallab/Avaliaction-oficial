a1a411b7b81d008d8ea8a82f7f1ff068027e5a51
Oscar <oscardikma@gmail.com>
2026-03-27 16:46:44 -0300

    fix:alteração do asdf

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,7 +1,14 @@
 "use client"
 
 import DashboardContent from "@/components/dashboard-content"
+import { Header } from "@/components/header"
 
 export default function AdminDashboardPage() {
-  return <DashboardContent />
+  return (
+  <>
+
+  <DashboardContent />
+  </>
+
+)
 }
